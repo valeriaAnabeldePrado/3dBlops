1ee572c6e22ece5c47a19b45de35424b6dacc26d
valeriaAnabeldePrado <anabeltayu@gmail.com>
2024-02-18 13:08:18 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // next.config.js
+
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(hdr)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next/",
+            outputPath: "static/images/", // Puedes ajustar la ruta de salida según tu estructura de carpetas
+            name: "[name].[ext]",
+            esModule: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
